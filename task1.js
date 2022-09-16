598fb80ba1fc8a5c2e1e31a6439988a212c584cc
Gunel S. Hasanzade <HasanzadeG@kapitalho.az>
2022-09-16 12:11:30 +0400

    add task 1

--- task1.js
+++ task1.js
@@ -1,8 +1,12 @@
+let n;
 function getDivisorsCnt(n) {
+  let arr = [];
   for (let i = 1; i <= n; i++) {
     if (n % i == 0) {
-      console.log(i);
+      arr.push(i);
     }
   }
+  return arr.length;
+  //   console.log(arr.length);
 }
 getDivisorsCnt(20);
