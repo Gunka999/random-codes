2445f0666766347f840bfca2e4507b35a26be550
Gunel S. Hasanzade <HasanzadeG@kapitalho.az>
2022-09-17 21:07:34 +0400

    update

--- task2.js
+++ task2.js
@@ -3,19 +3,24 @@ function getGrade(s1, s2, s3) {
   let cavab = "";
   let n = 3;
   let sum = (s1 + s2 + s3) / n;
-  if (sum >= 90 && sum <= 100) {
-    cavab = "A";
-  } else if (sum >= 80 && sum < 90) {
-    cavab = "B";
-  } else if (sum >= 70 && sum < 80) {
-    cavab = "C";
-  } else if (sum >= 60 && sum < 70) {
-    cavab = "D";
-  } else if (sum >= 0 && sum < 60) {
-    cavab = "F";
+  if (s1 < 0 || s2 < 0 || s3 < 0) {
+    console.log("kod sehvdir");
+  } else {
+    if (sum >= 90 && sum <= 100) {
+      cavab = "A";
+    } else if (sum >= 80 && sum < 90) {
+      cavab = "B";
+    } else if (sum >= 70 && sum < 80) {
+      cavab = "C";
+    } else if (sum >= 60 && sum < 70) {
+      cavab = "D";
+    } else if (sum >= 0 && sum < 60) {
+      cavab = "F";
+    }
   }
-  return cavab;
+
+  // return cavab;
   console.log(cavab);
 }
 
-getGrade(100, 50, 80);
+getGrade(100, -100, 80);
